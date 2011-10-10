7c64b68800ba5c76dd27e4ba8b34d7bbbe5cf249
Johan Ivarsson <johanivarsson@Johans-MacBook-Pro.local>
2011-10-10 23:21:43 +0200

    a small test program

---
+++ pgm1.m
@@ -0,0 +1,25 @@
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[]) {
+	
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	int sum;
+	int value1, value2;
+	int tal1, tal2;
+	
+	tal1 = 1;
+	tal2 = 3;
+	
+	value1 = 87;
+	value2 = 15;
+	
+	sum = value1 - value2;
+	
+	NSLog(@"Summan av %i minus %i är %i",value1, value2, sum);
+	NSLog(@"Tha fraction is %i/%i",tal1, tal2);
+    
+	
+    [pool drain];
+    return 0;
+}
